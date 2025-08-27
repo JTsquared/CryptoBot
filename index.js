c9f960c1ea3b28dd87d3b49287656115192c6dba
jtsquared <turner.e.jonathan@gmail.com>
2025-08-26 22:09:57 -0400

    configuring express

--- index.js
+++ index.js
@@ -89,6 +89,6 @@ app.use(express.json());
 app.use("/api/prizepool", prizePoolRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`API server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`API server running on http://0.0.0.0:${PORT}`);
 });
